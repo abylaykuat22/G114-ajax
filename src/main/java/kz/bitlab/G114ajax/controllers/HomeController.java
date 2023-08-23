7bcaeffe1579b4a15cdbc21457095e26f472ba11
abylaykuat22 <abylaykuat@gmail.com>
2023-08-23 21:07:27 +0600

    G114 generate excel file

--- src/main/java/kz/bitlab/G114ajax/controllers/HomeController.java
+++ src/main/java/kz/bitlab/G114ajax/controllers/HomeController.java
@@ -1,9 +1,17 @@
 package kz.bitlab.G114ajax.controllers;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import kz.bitlab.G114ajax.models.Academy;
 import kz.bitlab.G114ajax.repositories.AcademyRepository;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -13,6 +21,23 @@ public class HomeController {
 
   @GetMapping("/")
   public String homePage() {
+    List<Academy> academies = academyRepository.findAll();
+    try (Workbook workbook = new XSSFWorkbook()) {
+      Sheet sheet = workbook.createSheet("Sheet 1");
+      int i = 0;
+      for (Academy academy : academies) {
+        Row headerRow = sheet.createRow(i);
+        Cell headerCell = headerRow.createCell(0);
+        headerCell.setCellValue(academy.getName());
+        i++;
+      }
+      try (FileOutputStream fileOut = new FileOutputStream("workbook.xlsx")) {
+        workbook.write(fileOut);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
     return "home";
   }
 
